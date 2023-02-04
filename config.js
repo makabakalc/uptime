64edd08bff3fb0e152f8eb9c33c8cd355c85c5c4
makabakalc <1292445872@qq.com>
2023-02-04 21:09:24 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'makabaka's Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm793642263-7fdfab8136ac3b82e95fb5dc',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'GitHub',
+      url: 'github.makabaka.tk'
+    },
+
+  ],
+};
